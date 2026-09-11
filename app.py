f4f3ff8fd36d60f88fdaf5162c696b056f345fd7
georgeheartnum-debug <georgeheartnum@gmail.com>
2026-09-11 07:39:53 -1000

    Update app.py

--- app.py
+++ app.py
@@ -84,6 +84,6 @@ def delete(id):
     db.session.commit()
     return redirect('/dashboard')
 
-if _name_ == '_main_':
+if __name__ == '__main__':
     port = int(os.environ.get("PORT", 10000))
     app.run(host='0.0.0.0', port=port)
